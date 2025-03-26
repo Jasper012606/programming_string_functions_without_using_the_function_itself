685f0ca06514e80147264937f22913b7b2f21029
Jasper <japeemga@gmail.com>
2025-03-27 00:44:07 +0800

    Check each letter, if in lowercase, use upper() then add to swapcased_input, then print

--- imitate_swapcase()_function.py
+++ imitate_swapcase()_function.py
@@ -3,8 +3,12 @@ user_input = input("Enter first name: ")
 #set swapcased input to ""
 swapcased_input = ""
 #check for each letter of each word
+for char in user_input:
     #if lower case
+    if char.islower():
         #use upper() then add to swapcased input
+        swapcased_input += char.upper()
     #if uppercase
         #use lower() then add to swapcased input
 #print swapcased input
+print(swapcased_input)
