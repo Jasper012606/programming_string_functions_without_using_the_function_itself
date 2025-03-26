749609da2e0967292bec2db0e686e69f7d383738
Jasper <japeemga@gmail.com>
2025-03-27 00:40:39 +0800

    Ask for input, set swapcased input to ""

--- imitate_swapcase()_function.py
+++ imitate_swapcase()_function.py
@@ -1,5 +1,7 @@
 #ask for input
+user_input = input("Enter first name: ")
 #set swapcased input to ""
+swapcased_input = ""
 #check for each letter of each word
     #if lower case
         #use upper() then add to swapcased input
