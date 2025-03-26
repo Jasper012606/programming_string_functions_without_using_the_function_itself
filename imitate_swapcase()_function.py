fd89e4d914591c872ffe06958131fcc0c1d7e583
Jasper <japeemga@gmail.com>
2025-03-27 00:45:28 +0800

    If in uppercase, use lower() then add to swapcased input, then print

--- imitate_swapcase()_function.py
+++ imitate_swapcase()_function.py
@@ -9,6 +9,8 @@ for char in user_input:
         #use upper() then add to swapcased input
         swapcased_input += char.upper()
     #if uppercase
+    if char.isupper():
         #use lower() then add to swapcased input
+        swapcased_input += char.lower()
 #print swapcased input
 print(swapcased_input)
