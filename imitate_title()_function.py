25b307f248583e443fac10ad34604a5c6287d575
Jasper <japeemga@gmail.com>
2025-03-27 00:16:04 +0800

    Input the split input

--- imitate_title()_function.py
+++ imitate_title()_function.py
@@ -1,3 +1,4 @@
 #ask for input then use split() function
+user_input = input("Enter your name: ").split()
 #make the first letter for each word uppercase and remaining letters lower case
 #print tinput in titled casing
