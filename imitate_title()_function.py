25ce42d7dc23dd6456d51779209355d2c54098db
Jasper <japeemga@gmail.com>
2025-03-27 00:19:26 +0800

    Update in pseudocode - set title-cased input to ""

--- imitate_title()_function.py
+++ imitate_title()_function.py
@@ -1,4 +1,6 @@
 #ask for input then use split() function
 user_input = input("Enter your name: ").split()
+#set the input in titled casing variable to ""
+titled_casing = ""
 #make the first letter for each word uppercase and remaining letters lower case
 #print tinput in titled casing
