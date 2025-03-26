6fd676d19ed8a79df799baa69593a62cf15293ab
Jasper <japeemga@gmail.com>
2025-03-27 00:26:00 +0800

    Make every first letter of each word uppercase then remaining letters lowercase

--- imitate_title()_function.py
+++ imitate_title()_function.py
@@ -2,5 +2,7 @@
 user_input = input("Enter your name: ").split()
 #set the input in titled casing variable to ""
 titled_casing = ""
-#make the first letter for each word uppercase and remaining letters lower case
+#make the first letter for each word uppercase and remaining letters lower case then add to titled_casing
+for word in user_input:
+    titled_casing += (word[0].upper() + word[1:].lower()) + " " #Add " " to seperate each word
 #print tinput in titled casing
