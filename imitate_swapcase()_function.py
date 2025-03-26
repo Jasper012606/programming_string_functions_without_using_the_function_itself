9b7ff7505bd7f89b0fd17851865c4d3b278e6649
Jasper <japeemga@gmail.com>
2025-03-27 00:38:15 +0800

    Pseudocode

---
+++ imitate_swapcase()_function.py
@@ -0,0 +1,8 @@
+#ask for input
+#set swapcased input to ""
+#check for each letter of each word
+    #if lower case
+        #use upper() then add to swapcased input
+    #if uppercase
+        #use lower() then add to swapcased input
+#print swapcased input
