3fd9b3b49d8ac564625e34cce5364c8960107e5a
Jasper <japeemga@gmail.com>
2025-03-28 02:01:37 +0800

    Pseudocode

---
+++ imitate_isupper()_function.py
@@ -0,0 +1,6 @@
+#ask for input
+user_input = input("Enter: ")
+#compare if the input is the same as input in uppercase
+check_if_in_uppercase = user_input == user_input.upper()
+#print
+print(check_if_in_uppercase)
