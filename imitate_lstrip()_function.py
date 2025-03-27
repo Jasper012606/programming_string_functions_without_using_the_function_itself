d0586b25726d586b7ff11f3098a0b54825b1e618
Jasper <japeemga@gmail.com>
2025-03-28 01:46:50 +0800

    Ask for user input, initialize space_count to 0

--- imitate_lstrip()_function.py
+++ imitate_lstrip()_function.py
@@ -1,5 +1,7 @@
 #ask user for input
+user_input = input("Enter your name with few spaces: ")
 #initialize space_count to 0
+space_count = 0
 #check the characters of the input
     #add 1 to space_count if character is space
     #break if character is alpha
