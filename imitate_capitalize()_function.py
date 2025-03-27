a9c0e4b2163eeaba7e6eff954a8d949c6cc282ab
Jasper <japeemga@gmail.com>
2025-03-28 01:13:10 +0800

    Make the first letter of the input in uppercase and the rest in lowercase, then set that as the capitalized input then print

--- imitate_capitalize()_function.py
+++ imitate_capitalize()_function.py
@@ -3,5 +3,8 @@ user_input = input("Enter a statement: ")
 #set the capitalized_input to ""
 capitalized_input = ""
 #make the first letter of the input in uppercase and the rest in lowercase
+for char in user_input:
     #set the modified input to capitalized_input
+    capitalized_input = (user_input[0].upper() + user_input[1:].lower())
 #print the capitalized input
+print(capitalized_input)
