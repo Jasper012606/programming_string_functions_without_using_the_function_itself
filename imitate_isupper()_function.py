83e1de773241738a05bea5a0fde6445736c58c54
Jasper <japeemga@gmail.com>
2025-03-28 02:02:45 +0800

    Ask for user input

--- imitate_isupper()_function.py
+++ imitate_isupper()_function.py
@@ -1,6 +1,4 @@
 #ask for input
-user_input = input("Enter: ")
+user_input = input("Enter a word/statement: ")
 #compare if the input is the same as input in uppercase
-check_if_in_uppercase = user_input == user_input.upper()
-#print
-print(check_if_in_uppercase)
+#print result
