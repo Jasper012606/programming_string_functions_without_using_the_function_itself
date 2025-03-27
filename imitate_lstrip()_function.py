9e98f9c931ebb84578e301e6defe3e822e1617fa
Jasper <japeemga@gmail.com>
2025-03-28 01:49:24 +0800

    Check each character in input, if space add 1 to space count, break if alpha, print input from space_count

--- imitate_lstrip()_function.py
+++ imitate_lstrip()_function.py
@@ -3,6 +3,12 @@ user_input = input("Enter your name with few spaces: ")
 #initialize space_count to 0
 space_count = 0
 #check the characters of the input
+for char in user_input:
     #add 1 to space_count if character is space
+    if char.isspace():
+        space_count += 1
     #break if character is alpha
+    else:
+        break
 #print the input from space_count
+print("Output:", user_input[space_count:])
