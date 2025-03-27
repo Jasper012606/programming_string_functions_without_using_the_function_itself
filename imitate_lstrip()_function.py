250b05cfc996298d00c438bbbda7722f74a93ca1
Jasper <japeemga@gmail.com>
2025-03-28 01:33:43 +0800

    Pseudocode

---
+++ imitate_lstrip()_function.py
@@ -0,0 +1,6 @@
+#ask user for input
+#initialize space_count to 0
+#check the characters of the input
+    #add 1 to space_count if character is space
+    #break if character is alpha
+#print the input from space_count
