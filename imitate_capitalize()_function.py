d453351f3675188ad046a62d0080c0cfefe2e3cf
Jasper <japeemga@gmail.com>
2025-03-28 01:09:54 +0800

    Pseudocode

---
+++ imitate_capitalize()_function.py
@@ -0,0 +1,5 @@
+#ask user for input
+#set the capitalized_input to ""
+#make the first letter of the input in uppercase and the rest in lowercase
+    #set the modified input to capitalized_input
+#print the capitalized input
