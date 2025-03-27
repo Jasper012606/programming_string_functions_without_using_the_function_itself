5c60ebd72ec871694a43675e4c866b6af15a06d1
Jasper <japeemga@gmail.com>
2025-03-28 02:04:36 +0800

    Compare if input is the same as input in uppercase, then print result

--- imitate_isupper()_function.py
+++ imitate_isupper()_function.py
@@ -1,4 +1,6 @@
 #ask for input
 user_input = input("Enter a word/statement: ")
 #compare if the input is the same as input in uppercase
+result = user_input == user_input.upper()
 #print result
+print(result)
