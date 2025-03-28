c561e11abfb5a8ff7447372f9de50a0d995403c6
Jasper <japeemga@gmail.com>
2025-03-29 04:02:45 +0800

    Check if prefix matches the prefix of the input, print True if true, print False if not true

--- compilation_of_programs_batch_7/imitate_startswith()_function.py
+++ compilation_of_programs_batch_7/imitate_startswith()_function.py
@@ -3,5 +3,9 @@ user_input = input("Enter a word: ")
 #ask what prefix the user wants to check
 prefix_to_check = input("You want to check if your word starts with: ")
 #check if the prefix matches the prefix of the input
+if len(prefix_to_check) <= len(user_input) and user_input[0:len(prefix_to_check)] == prefix_to_check:
     #print True if true
+    print(True)
     #print Fale if not true
+else:
+    print(False)
