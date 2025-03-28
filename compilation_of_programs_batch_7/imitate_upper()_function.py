21d053fb5c8478f5e3819142ce8b1e33b5cbcc30
Jasper <japeemga@gmail.com>
2025-03-29 03:49:09 +0800

    Ask for input then lowercase all character, use swapcase, then print

--- compilation_of_programs_batch_7/imitate_upper()_function.py
+++ compilation_of_programs_batch_7/imitate_upper()_function.py
@@ -1,3 +1,6 @@
 #ask for input then lowercase all characters
+user_input = input("Enter a word: ").lower()
 #set uppercased input as input then swapcase
+uppercased_input = user_input.swapcase()
 #print uppercased input
+print(uppercased_input)
