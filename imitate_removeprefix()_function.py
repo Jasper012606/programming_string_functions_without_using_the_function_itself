cd908fe99214bf529ac4543fa327343248879789
Jasper <japeemga@gmail.com>
2025-03-29 02:18:34 +0800

    Pseudocode

---
+++ imitate_removeprefix()_function.py
@@ -0,0 +1,5 @@
+#ask for input
+#ask what prefix the user wants to remove
+#check if the prefix matches the prefix of the user input
+#print input with prefix removed if true
+#print "Prefix does not match" if not 
