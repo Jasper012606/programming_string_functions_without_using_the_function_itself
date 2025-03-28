c3ca629f4d3fea100132d57fc89ca9d2f6656a8d
Jasper Gabriel <japeemga@gmail.com>
2025-03-29 04:05:06 +0800

    Fixed the logic

--- compilation_of_programs_batch_6/imitate_ljust()_function.py
+++ compilation_of_programs_batch_6/imitate_ljust()_function.py
@@ -3,6 +3,6 @@ user_input = input("Enter any word: ")
 #ask for space to be added
 space_to_add = int(input("Enter how much left adjustment you want: "))
 #set adjusted input as space plus the input
-adjusted_input = " " * space_to_add + user_input
+adjusted_input = user_input + " " * space_to_add 
 #print the adjusted input
 print(adjusted_input)
