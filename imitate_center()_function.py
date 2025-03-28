b44e37a32e697e729c9d420993d5fa21d0a8ee93
Jasper <japeemga@gmail.com>
2025-03-29 01:34:29 +0800

    Pseudocode

---
+++ imitate_center()_function.py
@@ -0,0 +1,8 @@
+#ask for user input
+#ask the user for the width
+#get the total space needed
+#check if total space is greater than 0
+    #if true, get the total space for both sides
+        #set centered text plus the spaces
+    #else, print text as is
+#print centered text
