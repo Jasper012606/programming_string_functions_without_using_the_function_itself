dd3e0354e06fd3a824278d9e0f443dffb0b5fe06
Jasper <japeemga@gmail.com>
2025-03-29 02:23:36 +0800

    Check if prefix matches the prefix of the user input, print input without the prefix if true, print the user input if not true

--- imitate_removeprefix()_function.py
+++ imitate_removeprefix()_function.py
@@ -3,5 +3,9 @@ user_input = input("Enter a word: ")
 #ask what prefix the user wants to remove
 prefix_to_remove = input("Enter the prefix you want to remove: ")
 #check if the prefix matches the prefix of the user input
-#print input with prefix removed if true
-#print "Prefix does not match" if not 
+if len(prefix_to_remove) <= len(user_input) and user_input[0:len(prefix_to_remove)] == prefix_to_remove:
+    #print input with prefix removed if true
+    print(user_input[len(prefix_to_remove):])
+    #print user input if not 
+else:
+    print(user_input)
