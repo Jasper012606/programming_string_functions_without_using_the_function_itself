d130b3f93fec52f3571b1c4939ed552da5a00998
Jasper <japeemga@gmail.com>
2025-03-29 03:44:09 +0800

    Check if suffix matches the suffix of the user input, print user input without the suffix if true, print the user input as is if not true

--- compilation_of_programs_batch_7/imitate_removesuffix()_function.py
+++ compilation_of_programs_batch_7/imitate_removesuffix()_function.py
@@ -3,5 +3,9 @@ user_input = input("Enter a word: ")
 #ask what suffix the user wants to remove
 suffix_to_remove = input("Enter the suffix want to remove: ")
 #check if suffix matches the suffix of the input
+if len(suffix_to_remove) <= len(user_input) and user_input[-len(suffix_to_remove):] == suffix_to_remove:
     #print input without the suffix if true
+    print(user_input[0:-len(suffix_to_remove)])
     #print input as is if not true
+else:
+    print(user_input)
