c5868d19cc8da110a6b6dc4dc2863da449cd1a85
Jasper <japeemga@gmail.com>
2025-03-29 03:58:06 +0800

    Ask for input, ask what prefix the user wants to check

--- compilation_of_programs_batch_7/imitate_startswith()_function.py
+++ compilation_of_programs_batch_7/imitate_startswith()_function.py
@@ -1,5 +1,7 @@
 #ask for input
+user_input = input("Enter a word: ")
 #ask what prefix the user wants to check
+prefix_to_check = input("You want to check if your word starts with: ")
 #check if the prefix matches the prefix of the input
     #print True if true
     #print Fale if not true
