9cf2a57d6d7179629665c69b4705b29f1f9f2ced
Jasper <japeemga@gmail.com>
2025-03-29 01:52:06 +0800

    set adjusted input as space plus the user input

--- imitate_ljust()_function.py
+++ imitate_ljust()_function.py
@@ -3,4 +3,6 @@ user_input = input("Enter any word: ")
 #ask for space to be added
 space_to_add = int(input("Enter how much left adjustment you want: "))
 #set adjusted input as space plus the input
+adjusted_input = " " * space_to_add + user_input
 #print the adjusted input
+print(adjusted_input)
