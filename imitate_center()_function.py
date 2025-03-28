51078e886db3ec62423e5cf39a2e783067a3cee0
Jasper <japeemga@gmail.com>
2025-03-29 01:38:27 +0800

    Check if total space is greater than 0, if true, get spaces for both sides, if not true, print input as is

--- imitate_center()_function.py
+++ imitate_center()_function.py
@@ -5,7 +5,12 @@ width = int(input("Enter the width: "))
 #get the total space needed
 total_space = width - len(user_input)
 #check if total space is greater than 0
+if total_space > 0:
     #if true, get the total space for both sides
+    left_space = total_space // 2
+    right_space = total_space - left_space
         #set centered text plus the spaces
     #else, print text as is
+else:
+    print(user_input)
 #print centered text
