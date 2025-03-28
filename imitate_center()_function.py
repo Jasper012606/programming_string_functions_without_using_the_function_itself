69896b7dad1b9f2d39ff65c5e5d77429d8c3fe65
Jasper <japeemga@gmail.com>
2025-03-29 01:36:24 +0800

    Ask for input, width, and get the total spaces

--- imitate_center()_function.py
+++ imitate_center()_function.py
@@ -1,6 +1,9 @@
 #ask for user input
+user_input = input("Enter any word: ")
 #ask the user for the width
+width = int(input("Enter the width: "))
 #get the total space needed
+total_space = width - len(user_input)
 #check if total space is greater than 0
     #if true, get the total space for both sides
         #set centered text plus the spaces
