e7d8484f894ce16aca68f5170c36157c69a87b7d
Jasper <japeemga@gmail.com>
2025-03-29 02:06:35 +0800

    Ask user for input, then ask what suffix the user wants to check

--- imitate_endswith()_function.py
+++ imitate_endswith()_function.py
@@ -1,5 +1,7 @@
 #ask for input
+user_input = input("Enter a word: ")
 #ask what suffix the user wants to check
+suffix_to_check = input("You want to check if your word ends with: ")
 #check if the suffix matches the suffix of the user input
     #print True if true
     #print False if not true
