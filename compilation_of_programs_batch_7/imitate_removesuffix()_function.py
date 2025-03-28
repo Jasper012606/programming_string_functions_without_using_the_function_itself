b668991a8c975d01668bf13ecfa18f7a6583b992
Jasper <japeemga@gmail.com>
2025-03-29 03:25:07 +0800

    Pseudocode

---
+++ compilation_of_programs_batch_7/imitate_removesuffix()_function.py
@@ -0,0 +1,5 @@
+#ask for input
+#ask what suffix the user wants to remove
+#check if suffix matches the suffix of the input
+    #print input without the suffix if true
+    #print input as is if not true
