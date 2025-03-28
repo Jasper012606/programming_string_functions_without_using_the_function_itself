925c05be4c1568116072ce4423333577d5728694
Jasper <japeemga@gmail.com>
2025-03-29 04:36:17 +0800

    Set adjusted input as spaces to add plus input, then print

--- compilation_of_programs_batch_7/imitate_rjust()_function.py
+++ compilation_of_programs_batch_7/imitate_rjust()_function.py
@@ -3,4 +3,6 @@ user_input = input("Enter a word: ")
 #ask for space to be added
 space_to_add = int(input("Enter how much right adjustment you want: "))
 #set adjusted input as space plus the input
+adjusted_input = " " * space_to_add + user_input
 #print adjusted input
+print(adjusted_input)
