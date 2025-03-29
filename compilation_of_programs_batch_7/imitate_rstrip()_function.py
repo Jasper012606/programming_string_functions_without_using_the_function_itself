86eba95fac847ebc4e7d6e8d6baf4cb5b55aa6b6
Jasper <japeemga@gmail.com>
2025-03-29 20:36:38 +0800

    Reverse the string input, remove leading spaces(trailing spacesin reversed), then reverse the string input again, then print

--- compilation_of_programs_batch_7/imitate_rstrip()_function.py
+++ compilation_of_programs_batch_7/imitate_rstrip()_function.py
@@ -1,4 +1,6 @@
 #ask for input
 user_input = input("Enter a word with trailing spaces: ")
 #reverse the string input, remove the spaces and reverse again
+input_without_trailing_spaces = user_input[::-1].lstrip()[::-1]
 #print input without trailing spaces
+print(input_without_trailing_spaces)
