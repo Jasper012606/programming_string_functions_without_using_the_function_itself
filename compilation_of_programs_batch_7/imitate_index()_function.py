2f725f735ef6abc8da675462472f5e4e607797b5
Jasper <japeemga@gmail.com>
2025-03-29 13:00:58 +0800

    Ask for input, and ask what the user is looking for

--- compilation_of_programs_batch_7/imitate_index()_function.py
+++ compilation_of_programs_batch_7/imitate_index()_function.py
@@ -1,5 +1,7 @@
 #ask for input
+user_input = input("Enter a word: ")
 #ask what user want to find
+what_to_find = input("What do you want to find: ")
 #use find() and set as result
 #if result is -1, print "ValueError: substring not found"
 #else print result
