0338f27ed79ce78e5f6e52baee43dcf9a275030f
Jasper <japeemga@gmail.com>
2025-03-29 13:23:50 +0800

    Ask for input, then ask what the user is looking for

--- compilation_of_programs_batch_7/imitate_rindex()_function.py
+++ compilation_of_programs_batch_7/imitate_rindex()_function.py
@@ -1,5 +1,7 @@
 #ask for input
-#ask what user wants to fimnd
-#use rfind() and set as result
+user_input = input("Enter a word: ")
+#ask what user wants to find
+what_to_find = input("What do you want to find: ")
+#use rfind() and set as resul
 #if result is -1, print "Value Error: substring not found"
 #else print result
