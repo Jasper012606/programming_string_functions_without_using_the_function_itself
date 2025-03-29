113c5f7ee215d93de0e19dc34af311b3109f8fd0
Jasper <japeemga@gmail.com>
2025-03-29 14:33:56 +0800

    Added a condition when input starts with '-'

--- compilation_of_programs_batch_7/imitate_zfill()_function.py
+++ compilation_of_programs_batch_7/imitate_zfill()_function.py
@@ -4,11 +4,25 @@ user_input = input("Enter a word: ")
 width = int(input("Enter width: "))
 #get the number of spaces left
 spaces = width - len(user_input )
-#if spaces are greater than 0
-if spaces > 0:
-    #print "0" times the number of spaces plus input
-    print("0" * spaces + user_input)
+#if string starts with "-"
+if user_input.startswith("-"):
+    #Adjust space for the negative symbol
+    spaces = width - len(user_input)
+    #if spaces are greater than 0
+    if spaces > 0:
+        #print the negative symbol plus "0" times spaces plus the input
+        print("-" + "0" * spaces + user_input[1:])
+    #else 
+    else:
+        #print the input
+        print(user_input)
 #else
 else:
-    #print the input
-    print(user_input)
+    #if spaces are greater than 0
+    if spaces > 0:
+        #print "0" times the number of spaces plus input
+        print("0" * spaces + user_input)
+    #else
+    else:
+        #print the input
+        print(user_input)
