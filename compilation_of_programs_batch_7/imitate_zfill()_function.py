a4710f5d0a3cbdf62a38342d06ffb27e8e8f2f6e
Jasper <japeemga@gmail.com>
2025-03-29 13:57:32 +0800

    If spaces are greater than 0, print "0" times the spaces then the input, else just print the input

--- compilation_of_programs_batch_7/imitate_zfill()_function.py
+++ compilation_of_programs_batch_7/imitate_zfill()_function.py
@@ -5,6 +5,10 @@ width = int(input("Enter width: "))
 #get the number of spaces left
 spaces = width - len(user_input )
 #if spaces are greater than 0
+if spaces > 0:
     #print "0" times the number of spaces plus input
+    print("0" * spaces + user_input)
 #else
+else:
     #print the input
+    print(user_input)
