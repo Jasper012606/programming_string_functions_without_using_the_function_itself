c692a8166a7713cd01a004bd8407b7f0329b8814
Jasper <japeemga@gmail.com>
2025-03-29 13:29:43 +0800

    Use rfind() function, if the result is -1, print 'ValueError: substring not found', else print the result

--- compilation_of_programs_batch_7/imitate_rindex()_function.py
+++ compilation_of_programs_batch_7/imitate_rindex()_function.py
@@ -2,6 +2,11 @@
 user_input = input("Enter a word: ")
 #ask what user wants to find
 what_to_find = input("What do you want to find: ")
-#use rfind() and set as resul
+#use rfind() and set as result
+result = user_input.rfind(what_to_find)
 #if result is -1, print "Value Error: substring not found"
-#else print result
+if result == -1:
+    print("ValueError: substring not found")
+#else print 
+else:
+    print(result)
