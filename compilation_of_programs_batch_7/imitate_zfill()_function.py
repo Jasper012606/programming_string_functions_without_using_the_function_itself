bc3de4ad02daa9c15b6bee49d1b736ece9e4c4ea
Jasper <japeemga@gmail.com>
2025-03-29 13:54:10 +0800

    Ask for input, width, and get the spaces left

--- compilation_of_programs_batch_7/imitate_zfill()_function.py
+++ compilation_of_programs_batch_7/imitate_zfill()_function.py
@@ -1,6 +1,9 @@
 #ask for input
+user_input = input("Enter a word: ")
 #ask for width
+width = int(input("Enter width: "))
 #get the number of spaces left
+spaces = width - len(user_input )
 #if spaces are greater than 0
     #print "0" times the number of spaces plus input
 #else
