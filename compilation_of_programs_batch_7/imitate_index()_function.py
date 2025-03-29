239c0c62dd23a4c05804e330a028d501c1f4ab0b
Jasper <japeemga@gmail.com>
2025-03-29 13:03:27 +0800

    Use find() function, if the result is -1, print 'ValueError: substring not found', else print the result

--- compilation_of_programs_batch_7/imitate_index()_function.py
+++ compilation_of_programs_batch_7/imitate_index()_function.py
@@ -3,5 +3,10 @@ user_input = input("Enter a word: ")
 #ask what user want to find
 what_to_find = input("What do you want to find: ")
 #use find() and set as result
+result = user_input.find(what_to_find)
 #if result is -1, print "ValueError: substring not found"
+if result == -1:
+    print("ValueError: substring not found")
 #else print result
+else:
+    print(result)
