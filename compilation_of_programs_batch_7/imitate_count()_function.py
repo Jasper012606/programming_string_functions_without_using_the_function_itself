bc16b60547c15579672b2bbf6b692678bc0654d4
Jasper <japeemga@gmail.com>
2025-04-01 23:25:41 +0800

    Check each character of the input, if input matches what the user wants to count, add 1 to count, finally return the count

--- compilation_of_programs_batch_7/imitate_count()_function.py
+++ compilation_of_programs_batch_7/imitate_count()_function.py
@@ -3,9 +3,13 @@ def imitate_count(input, what_to_count):
     #initialize count to 0
     count = 0
     #check for character from the input
+    for char in input:
         #if char matches what the user wants to count
+        if char == what_to_count:
             #add 1 to count
+            count += 1
     #return count
+    return count
 #ask for input
 #ask what the user wants to count from the input
 #call and print the function
