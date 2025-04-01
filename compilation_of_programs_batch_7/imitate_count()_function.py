ede0e8a5a26d00f29622a81d03d1c2681b84e1f0
Jasper <japeemga@gmail.com>
2025-04-01 23:30:55 +0800

    Ask for input, and what the user wants to count, then call and print the function

--- compilation_of_programs_batch_7/imitate_count()_function.py
+++ compilation_of_programs_batch_7/imitate_count()_function.py
@@ -11,5 +11,8 @@ def imitate_count(input, what_to_count):
     #return count
     return count
 #ask for input
+user_input = input("Enter a short phrase: ")
 #ask what the user wants to count from the input
+what_to_count = input("What do you want to count: ")
 #call and print the function
+print(imitate_count(user_input, what_to_count))
