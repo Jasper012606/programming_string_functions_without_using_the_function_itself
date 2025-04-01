7869aa0844b38597224e122f6553fb8102c86e54
Jasper <japeemga@gmail.com>
2025-04-01 23:23:58 +0800

    Make a function, initialize count to 0

--- compilation_of_programs_batch_7/imitate_count()_function.py
+++ compilation_of_programs_batch_7/imitate_count()_function.py
@@ -1,5 +1,7 @@
 #make a function
+def imitate_count(input, what_to_count):
     #initialize count to 0
+    count = 0
     #check for character from the input
         #if char matches what the user wants to count
             #add 1 to count
