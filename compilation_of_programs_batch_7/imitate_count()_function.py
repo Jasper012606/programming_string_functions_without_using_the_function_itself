e4c8ae010d848c2854558adef856fb521d1e44c5
Jasper <japeemga@gmail.com>
2025-04-01 23:21:38 +0800

    Pseudocode

---
+++ compilation_of_programs_batch_7/imitate_count()_function.py
@@ -0,0 +1,9 @@
+#make a function
+    #initialize count to 0
+    #check for character from the input
+        #if char matches what the user wants to count
+            #add 1 to count
+    #return count
+#ask for input
+#ask what the user wants to count from the input
+#call and print the function
